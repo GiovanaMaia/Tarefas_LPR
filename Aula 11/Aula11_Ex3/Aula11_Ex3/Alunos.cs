e5b67aa16c96fe83db7a93c87db104e3285ecbfe
GiovanaMaia <giovanamaia851@gmail.com>
2024-10-29 21:53:04 -0300

    Exercício 3
    Código do exercício 3- Aula11

---
+++ Aula 11/Aula11_Ex3/Aula11_Ex3/Alunos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula11_Ex3
+{
+    internal class Alunos
+    {
+        public int matricula;
+        public string nome;
+        public float prova1;
+        public float prova2;
+        public float trabalho;
+        public double media;
+        public double resto;
+
+
+        public void mediaFinal()
+        {
+
+            media = (prova1 * 2.5 + prova2 * 2.5 + trabalho * 2) / 3;
+
+        }
+        public int provaFinal()
+        {   //media final >= 60
+
+            if (media <= 60)
+            {
+                resto = 60 - media;
+                Console.WriteLine("Sua nota na prova final precisar ser <= " + resto % .2f);
+                return 1;
+            }
+            else return 0;
+        }
+    }
+}
---
+++ Aula 11/Aula11_Ex3/Aula11_Ex3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula11_Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alunos aluno = new Alunos();
+
+
+            Console.WriteLine("Digite o nº de matrícula: ");
+            aluno.matricula = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o nome do aluno: ");
+            aluno.nome = Console.ReadLine();
+            Console.WriteLine("Nota da prova 1: ");
+            aluno.prova1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nota da prova 2: ");
+            aluno.prova2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nota do trabalho: ");
+            aluno.trabalho = int.Parse(Console.ReadLine());
+
+            aluno.mediaFinal();
+            aluno.provaFinal();
+        }
+    }
+}
