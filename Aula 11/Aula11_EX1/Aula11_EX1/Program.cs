e670c1bd2dd8480eb73f27e981acdf1f73438e11
GiovanaMaia <giovanamaia851@gmail.com>
2024-10-29 21:40:12 -0300

    Exercício 1
    código do Exercício 1, aula 11

---
+++ Aula 11/Aula11_EX1/Aula11_EX1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula11_EX1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo novoRetangulo = new Retangulo();
+
+            Console.WriteLine("Digite a altura do retângulo");
+            novoRetangulo.Altura = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a largura:");
+            novoRetangulo.Largura = int.Parse(Console.ReadLine());
+            Console.WriteLine("entrou nas functions");
+            novoRetangulo.calcArea();
+            novoRetangulo.calcPerimetro();
+            Console.WriteLine("entrou das functions");
+        }
+    }
+}
---
+++ Aula 11/Aula11_EX1/Aula11_EX1/Retangulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula11_EX1
+{
+    internal class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public void calcArea()
+        {
+            double area = 0;
+
+            area = Altura * Largura;
+
+            Console.WriteLine("A área é igual a " + area);
+
+        }
+
+        public void calcPerimetro()
+        {
+            double perimetro = 0;
+
+            perimetro = 2 * Altura + 2 * Largura;
+
+            Console.WriteLine("O perímetro é igual a " + perimetro);
+        }
+    }
+}
