a10ecb032ed792a8633e417659ed5a3540819188
GiovanaMaia <giovanamaia851@gmail.com>
2024-10-29 21:51:47 -0300

    Exercício 2
    Código do exercício 2- Aula11

---
+++ Aula 11/Aula11_Ex2/Aula11_Ex2/Funcionario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula11_Ex2
+{
+    internal class Funcionario
+    {
+        public string nome;
+        public double salarioBrut;
+        public double salarioLiq;
+
+        public void aumentoSalario()
+        {
+            if (salarioBrut <= 2000)
+            {
+                salarioLiq = salarioBrut * 1.1;
+            }
+            else if (salarioBrut > 2000 && salarioBrut < 3000)
+            {
+                salarioLiq = salarioBrut * 1.15;
+            }
+            else
+            {
+                salarioLiq = salarioBrut * 1.20;
+            }
+
+        }
+        public void dados()
+        {
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Salário líquido: R$" + salarioLiq + ",00");
+
+        }
+
+    }
+}
---
+++ Aula 11/Aula11_Ex2/Aula11_Ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula11_Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.WriteLine("Digite o nome do funcionário: ");
+            funcionario.nome = Console.ReadLine();
+            Console.WriteLine("Digite o salário: ");
+            funcionario.salarioBrut = int.Parse(Console.ReadLine());
+
+            funcionario.aumentoSalario();
+            funcionario.dados();
+
+        }
+    }
+}
